7d9aa304ddd695ced3a3d2096fd07ed2dd2e5463
Paula Albarracin <105725991+xxpaulicoderxx@users.noreply.github.com>
2026-03-13 12:54:51 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'tocc-def-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar: guardar archivos en cache
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activar: limpiar caches viejas
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch: responder desde cache (funciona offline)
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
